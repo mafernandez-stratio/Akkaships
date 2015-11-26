6089021e71705ef2665234994edc1b0f20bca7df
jjlopezm <jjlopez@stratio.com>
2015-11-26 21:44:19 +0100

    Fix name of ship

--- Server/src/main/scala/es/codemotion/akkaships/server/Server.scala
+++ Server/src/main/scala/es/codemotion/akkaships/server/Server.scala
@@ -26,7 +26,8 @@ class Server extends Daemon with ServerConfig {
     val statisticsRouter: ActorRef = system.actorOf(RoundRobinPool(5).props(Props[StatisticsActor]), "statisticRouter")
 
     /* Ship Actors  */
-    system.actorOf(Props(new ShipActor(Ship(Position(1, 2), Vertical, 4), boardActor, statisticsRouter)), "Portaviones")
+    system.actorOf(Props(new ShipActor(Ship(Position(1, 2), Vertical, 4), boardActor, statisticsRouter)),
+      "Portaaviones")
     system.actorOf(Props(new ShipActor(Ship(Position(10, 10), Horizontal, 2), boardActor, statisticsRouter)),"Lancha")
 
     /* Ships Broadcast Router */
